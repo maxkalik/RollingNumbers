554f6dd97100f328f0a60a2e02ff775784c3eeb2
maxkalik <maxkalik@gmail.com>
2023-03-27 08:15:23 -0700

    documentation updated

--- Sources/RollingNumbers/CharLayer.swift
+++ Sources/RollingNumbers/CharLayer.swift
@@ -37,11 +37,7 @@ final class CharLayer: CATextLayer {
         string = String(char)
         alignmentMode = .center
         isWrapped = true
-//        truncationMode = .middle
 
-//        borderColor = UIColor.red.cgColor
-//        borderWidth = 1
-        
         // It’s perfect for objects that animate around the screen but don’t change in appearance
         shouldRasterize = true
     }
--- Sources/RollingNumbers/RollingNumbers.swift
+++ Sources/RollingNumbers/RollingNumbers.swift
@@ -6,28 +6,61 @@ public final class RollingNumbersView: UIView {
 
     // MARK: - Public
     
+    /// Animation that affect on numbers
     public enum AnimationType {
-        case allDigits
-        case onlyChangedDigits
-        case allAfterFirstChangedDigit
+        /// All numbers roll if even only one number change
+        case allNumbers
+        
+        /// Only changed numbers roll
+        case onlyChangedNumbers
+        
+        /// All numbers roll after first changed number
+        case allAfterFirstChangedNumber
+        
+        /// Numbers change without animation
         case noAnimation
     }
     
+    /// Rolling animation direction
     public enum RollingDirection {
+        
+        /// Rolling animation direction from up to down
         case down
+        
+        /// Rolling animation direction from down to up
         case up
     }
 
+    /// Numbers alignemnt
     public enum Alignment {
+        
+        /// Numbers aligned at the center of the view
         case center
+        
+        /// Numbers aligned by the left side of the view
         case left
+        
+        /// Numbers aligned by the right side of the view
         case right
     }
     
+    /// Rolling Animation Configuration
     public struct AnimationConfiguration {
+        
+        /// Rolling animation duration `CFTimeInterval`
+        /// Default value: `1`
         public var duration: CFTimeInterval
+        
+        /// Rolling animation speed `Float`
+        /// Default value: `0.3`
         public var speed: Float
+        
+        /// Rolling animation damping `CGFloat`
+        /// Default value: `17`
         public var damping: CGFloat
+        
+        /// Rolling animation initialVelocity `CGFloat`
+        /// Default initialVelocity: `1`
         public var initialVelocity: CGFloat
         
         public init(
@@ -43,26 +76,57 @@ public final class RollingNumbersView: UIView {
         }
     }
     
+    /// Rolling Animation Configuration of type `AnimationConfiguration` (check `RollingNumbersView.AnimationConfiguration`)
+    /// - Default value: `AnimationConfiguration()`
     public var animationConfiguration: AnimationConfiguration = AnimationConfiguration()
-    public var animationType: AnimationType? = .allAfterFirstChangedDigit
+    
+    /// Animation that affect on numbers. Default value `.allAfterFirstChangedNumber`
+    public var animationType: AnimationType? = .allAfterFirstChangedNumber
+    
+    /// Rolling animation direction
     public var rollingDirection: RollingDirection?
+    
+    /// Numbers alignemnt  of type `Alignment` (check `RollingNumbersView.Alignment`.
+    /// - Default value: `.left`
     public var alignment: Alignment = .left
+    
+    /// Character spacing  of type `CGFloat`
+    /// - Default value: `1`
     public var characterSpacing: CGFloat = 1
+    
+    /// Numbers font  of type `UIFont`.
+    /// - Default value `.systemFont(ofSize: 24, weight: .bold)`
     public var font = UIFont.systemFont(ofSize: 24, weight: .bold)
+    
+    /// Text color of type `UIColor`.
+    /// - Default value: `.black`
     public var textColor: UIColor = .black
+    
+    /// User Number Formatter to configure numbers
     public var formatter: NumberFormatter?
     
+    /// Set text color.
+    /// - Parameters:
+    ///     - textColor: Text color of type `UIColor`
+    ///     - withAnimationDuration: Optional transition duration between existing color and future one. Type `CFTimeInterval`. Default value: `nil`, means without duration.
     public func setTextColor(_ textColor: UIColor, withAnimationDuration duration: CFTimeInterval? = nil) {
         self.textColor = textColor
         self.setForegroundColor(textColor, withAnimationDuration: duration)
     }
     
+    /// Set number without animation
+    /// - Parameters:
+    ///     - double: Number as `Double`
     public func setNumber(_ double: Double) {
         self.number = double
         prepareColumns(rollingDirection: rollingDirection)
         updateColumns(animationType: .noAnimation)
     }
 
+    /// Set number with rolling animation
+    /// - Parameters:
+    ///     - double: Number as `Double`
+    ///     - animationType: check AnimatonType. Default value: `
     public func setNumberWithAnimation(
         _ double: Double,
         animationType: AnimationType? = nil,
@@ -80,10 +144,13 @@ public final class RollingNumbersView: UIView {
         )
     }
 
+    /// Text representation of the number
+    /// - Note: Read only
     public var text: String {
         prepareText(from: number)
     }
 
+    /// Initialization with frame when default number will be `0`
     public override init(frame: CGRect) {
         self.number = 0
         super.init(frame: frame)
@@ -91,6 +158,9 @@ public final class RollingNumbersView: UIView {
         setupCommon()
     }
     
+    /// Initialization with number.
+    /// - Parameter:
+    ///     - number: `Double`
     public init(number: Double = 0) {
         self.number = number
         super.init(frame: .zero)
@@ -98,20 +168,20 @@ public final class RollingNumbersView: UIView {
         setupCommon()
     }
     
-    private var width: CGFloat {
+    /// Calculated with of all number elements
+    /// - Note: Read only
+    public var width: CGFloat {
         text.reduce(into: Double()) { partialResult, char in
             partialResult += prepareWidthOfChar(char)
         }
     }
     
-    private var height: CGFloat {
+    /// Height of the number element
+    /// - Note: Read only
+    public var height: CGFloat {
         frame.height
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     public override func layoutSubviews() {
         super.layoutSubviews()
         
@@ -119,6 +189,10 @@ public final class RollingNumbersView: UIView {
             prepareColumns()
             isInitialColumnsPrepared = true
         }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - Private
@@ -257,7 +331,7 @@ public final class RollingNumbersView: UIView {
             .enumerated()
             .forEach({ index, columnLayer in
                 switch animationType ?? self.animationType {
-                case .onlyChangedDigits:
+                case .onlyChangedNumbers:
                     if prevDigits.indices.contains(index), prevDigits[index] == columnLayer.digit {
                         columnLayer.setToDigit()
                     } else {
@@ -265,7 +339,7 @@ public final class RollingNumbersView: UIView {
                             self.setFinishAnimationIfNeeded(&isAnimationFinished, completion: completion)
                         }
                     }
-                case .allAfterFirstChangedDigit:
+                case .allAfterFirstChangedNumber:
                     if prevDigits.indices.contains(index), prevDigits[index] != columnLayer.digit {
                         prevIndex = index
                     }
@@ -277,7 +351,7 @@ public final class RollingNumbersView: UIView {
                     } else {
                         columnLayer.setToDigit()
                     }
-                case .allDigits:
+                case .allNumbers:
                     columnLayer.setToDigit(isAnimated: true, config: animationConfiguration) {
                         self.setFinishAnimationIfNeeded(&isAnimationFinished, completion: completion)
                     }
